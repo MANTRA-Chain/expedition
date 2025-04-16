f1a65adaf90edf93de1cc05338eb705ad6289c68
₿igto <bigto@mantra.finance>
2025-04-16 23:54:53 +0800

    feat; Refactor useChainList hook to use dynamic hostname logic
    Replaced hardcoded chain data with logic that determines the chain list based on the current hostname. This improves flexibility and prepares the application for multi-environment support. Removed unused code and commented-out functionality for clarity and maintainability.

--- src/hooks/useChainList.ts
+++ src/hooks/useChainList.ts
@@ -1,50 +1,33 @@
-// import { useEffect } from "react";
-// import { uniqBy } from "lodash";
 import { IChain as Chain } from "../models/chain";
 import React from "react";
 
-// const mergeChainSets = (c1: Chain[], c2: Chain[]) => uniqBy(c1.concat(c2), "name");
-
 export default function() {
-  const [chains, setChains] = React.useState<Chain[]>([
-    {
-      name: "MANTRA EVM Canary",
-      network: "canary",
-      rpc: ["https://evm.archive.evm-canary.mantrachain.dev"],
-    },
-    {
-      name: "MANTRA EVM Mainnet",
-      network: "mainnet",
-      rpc: [""],
-    },
-    {
-      name: "MANTRA EVM Testnet",
-      network: "testnet",
-      rpc: [""],
-    },
-  ]);
-
-  // uncomment once we add 'chain list provider' concept. This list blows.
-
-  // useEffect(() => {
-  //   if (chains === undefined) {
-  //     fetch("https://chainid.network/chains.json")
-  //       .then((r) => r.json())
-  //       .then((chainIdNetwork) => {
-  //         const filteredChains = chainIdNetwork.filter((c: Chain) => {
-  //           if (c.rpc.length === 0) {
-  //             return false;
-  //           }
-  //           return true;
-  //         });
-  //         if (chains) {
-  //           setChains(mergeChainSets(chains, filteredChains));
-  //         } else {
-  //           setChains(filteredChains);
-  //         };
-  //       });
-  //   }
-  // }, [chains]);
+  // Check host name
+  const hostname = window.location.hostname;
+  let chainList: Chain[] = [];
+  if (hostname.includes("localhost") || hostname.includes(".mantrachain.dev")) {
+    chainList = [
+      {
+        name: "MANTRA EVM Canary",
+        network: "canary",
+        rpc: ["https://evm.archive.evm-canary.mantrachain.dev"],
+      },
+      {
+        name: "OMSTEAD Testnet",
+        network: "testnet",
+        rpc: ["https://evm.archive.omstead.io"],
+      },
+    ];
+  } else if (hostname.includes(".omstead.io")) {
+    chainList = [
+      {
+        name: "OMSTEAD Testnet",
+        network: "testnet",
+        rpc: ["https://evm.omstead.io"],
+      },
+    ];
+  }
+  const [chains, setChains] = React.useState<Chain[]>(chainList);
 
   return [chains, setChains];
 }
