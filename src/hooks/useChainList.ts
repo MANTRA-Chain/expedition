9b9c538928ef695e5d2438dfb8fcf4dde2f6eadb
₿igto <bigto@mantra.finance>
2025-04-17 11:15:42 +0800

    feat: add more chain info

--- src/hooks/useChainList.ts
+++ src/hooks/useChainList.ts
@@ -1,10 +1,22 @@
-import { IChain as Chain } from "../models/chain";
+import { IChain as Chain, INativeCurrency } from "../models/chain";
 import React from "react";
 
 export default function() {
   // Check host name
   const hostname = window.location.hostname;
-  let chainList: Chain[] = [];
+  let chainList: Chain[];
+  const nativeCurrency: INativeCurrency = {
+    name: "MANTRA",
+    symbol: "OM",
+    decimals: 18,
+  };
+  const defaultParams = {
+    nativeCurrency,
+    iconUrls: [
+      "https://raw.githubusercontent.com/cosmos/chain-registry/master/mantrachain/images/OM-Prim-Col.svg",
+      "https://raw.githubusercontent.com/cosmos/chain-registry/master/mantrachain/images/OM-Prim-Col.png",
+    ],
+  };
   if (
     hostname.includes("localhost")
     || hostname.includes(".mantrachain.dev")
@@ -12,22 +24,31 @@ export default function() {
   ) {
     chainList = [
       {
-        name: "MANTRA EVM Canary (Archive)",
-        network: "canary (Archive Node)",
-        rpc: ["https://evm.archive.evm-canary.mantrachain.dev"],
-      },
-      {
+        chainId: `0x${(7888).toString(16)}`,
         name: "OMSTEAD Testnet",
         network: "testnet (Archive Node)",
         rpc: ["https://evm.archive.omstead.io"],
+        blockExplorerUrls: ["https://explorer.omstead.io/evm"],
+        ...defaultParams,
+      },
+      {
+        chainId: `0x${(5887).toString(16)}`,
+        name: "MANTRA EVM Canary (Archive)",
+        network: "canary (Archive Node)",
+        rpc: ["https://evm.archive.evm-canary.mantrachain.dev"],
+        blockExplorerUrls: ["https://explorer.evm-canary.mantrachain.dev"],
+        ...defaultParams,
       },
     ];
   } else {
     chainList = [
       {
+        chainId: `0x${(7888).toString(16)}`,
         name: "OMSTEAD Testnet",
         network: "testnet",
         rpc: ["https://evm.omstead.io"],
+        blockExplorerUrls: ["https://explorer.evm-canary.mantrachain.dev"],
+        ...defaultParams,
       },
     ];
   }
